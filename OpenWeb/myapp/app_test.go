9d54043740c39a6b05743c932c9d51809d880042
Jun Woo <james2014479@gmail.com>
2024-11-02 11:26:12 +0900

    feat: test 환경구축 testify를 위한 goconvey와 assert 패키지설치

---
+++ OpenWeb/myapp/app_test.go
@@ -0,0 +1,80 @@
+package myapp
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexPathHandler(t *testing.T) {
+	assert := assert.New(t)
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello World", string(data))
+}
+
+func TestBarPathHandler_WithoutName(t *testing.T) {
+	assert := assert.New(t)
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello World!", string(data))
+}
+
+func TestBarPathHandler_WithName(t *testing.T) {
+	assert := assert.New(t)
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar?name=james", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello james!", string(data))
+}
+
+func TestFooHandler_WithoutJson(t *testing.T) {
+	assert := assert.New(t)
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/foo", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+}
+
+func TestFooHandler_WithJson(t *testing.T) {
+	assert := assert.New(t)
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/foo",
+		strings.NewReader(`{"first_name": "james", "last_name":"lee", "email":"james@gamil.com"}`))
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+
+	user := new(User)
+	err := json.NewDecoder(res.Body).Decode(user)
+	assert.Nil(err)
+	assert.Equal("james", user.FirstName)
+	assert.Equal("lee", user.LastName)
+}
